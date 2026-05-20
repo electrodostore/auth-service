ce46fabd8da9168be876ec0c4ae56f11d4e30a58
Emanuel <emanuelatencia2407@gmail.com>
2026-05-20 18:34:10 -0500

    Crear DTOs para registrar y exponer permisos

---
+++ src/main/java/com/electrodostore/auth_service/dto/PermissionRequestDto.java
@@ -0,0 +1,7 @@
+package com.electrodostore.auth_service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+//DTO para transferir los datos de un permiso cuando se quiera hacer un registro de este
+public record PermissionRequestDto(@NotBlank String PermissionName) {
+}
---
+++ src/main/java/com/electrodostore/auth_service/dto/PermissionResponseDto.java
@@ -0,0 +1,5 @@
+package com.electrodostore.auth_service.dto;
+
+//Dto para exponer un permiso al cliente
+public record PermissionResponseDto(Long id, String name) {
+}
