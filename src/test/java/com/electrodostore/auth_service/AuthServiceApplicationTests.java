848be563c3568c3a07d5baff78ed843c85126c96
Emanuel <emanuelatencia2407@gmail.com>
2026-05-20 18:00:32 -0500

    Crear primer commit con archivos basicos del proyecto

---
+++ src/test/java/com/electrodostore/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electrodostore.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
