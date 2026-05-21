93c5bcc4746205e2b6e5fdfdf286453c72a1dba1
Emanuel <emanuelatencia2407@gmail.com>
2026-05-21 09:58:59 -0500

    Agregar campo de estado a DTO que expone usuarios

--- src/main/java/com/electrodostore/auth_service/dto/user/UserResponseDto.java
+++ src/main/java/com/electrodostore/auth_service/dto/user/UserResponseDto.java
@@ -6,5 +6,7 @@ import java.util.List;
 
 public record UserResponseDto (Long id,
                                String RoleName,
-                               List<RoleResponseDto> roles){
+                               List<RoleResponseDto> roles,
+                               Long clientId,
+                               boolean unabled){
 }
