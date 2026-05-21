d2df85f2fc71038be2c4315dc218bd7b0295cbbf
Emanuel <emanuelatencia2407@gmail.com>
2026-05-21 12:56:27 -0500

    Definir consulta para consultar un usuario por su nombre de usuario

--- src/main/java/com/electrodostore/auth_service/repository/IUserRepository.java
+++ src/main/java/com/electrodostore/auth_service/repository/IUserRepository.java
@@ -4,9 +4,14 @@ import com.electrodostore.auth_service.model.UserSec;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUserRepository extends JpaRepository<UserSec, Long> {
 
     //Método que Spring-Data-Jpa interpreta para verificar sin un username está registrado y asociado a algún usuario
     boolean existsByUsername(String username);
+
+    //Consulta usuario por username
+    Optional<UserSec> findByUsername(String username);
 }
