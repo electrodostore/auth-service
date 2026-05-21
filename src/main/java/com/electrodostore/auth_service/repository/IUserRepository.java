a4e26a80ae8df4d4b118a0559741d8b8f1ea385a
Emanuel <emanuelatencia2407@gmail.com>
2026-05-21 10:04:29 -0500

    Definir consulta personalizada para validar si un username está registrado en la base de datos

--- src/main/java/com/electrodostore/auth_service/repository/IUserRepository.java
+++ src/main/java/com/electrodostore/auth_service/repository/IUserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IUserRepository extends JpaRepository<UserSec, Long> {
+
+    //Método que Spring-Data-Jpa interpreta para verificar sin un username está registrado y asociado a algún usuario
+    boolean existsByUsername(String username);
 }
