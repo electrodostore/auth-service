77f2daf2cb2c47929fcdcee8c6d71a821b4c42f3
Emanuel <emanuelatencia2407@gmail.com>
2026-05-20 19:50:08 -0500

    Crear DTO para tranferir los datos de los roles en el registro y las consulta a estos

---
+++ src/main/java/com/electrodostore/auth_service/dto/role/RoleRequestDto.java
@@ -0,0 +1,9 @@
+package com.electrodostore.auth_service.dto.role;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
+
+public record RoleRequestDto(@NotBlank String roleName, @NotEmpty List<@NotBlank String> rolesNames) {
+}
---
+++ src/main/java/com/electrodostore/auth_service/dto/role/RoleResponseDto.java
@@ -0,0 +1,8 @@
+package com.electrodostore.auth_service.dto.role;
+
+import com.electrodostore.auth_service.dto.permission.PermissionResponseDto;
+
+import java.util.List;
+
+public record RoleResponseDto(Long id, String name, List<PermissionResponseDto> permissions) {
+}
