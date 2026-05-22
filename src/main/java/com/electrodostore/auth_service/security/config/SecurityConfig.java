2af5099d651657b1ebc3c5d0cb382f1afaa606eb
Emanuel <emanuelatencia2407@gmail.com>
2026-05-21 19:53:42 -0500

    Registrar Beans necesarios para el proceso de autenticación

--- src/main/java/com/electrodostore/auth_service/security/config/SecurityConfig.java
+++ src/main/java/com/electrodostore/auth_service/security/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.electrodostore.auth_service.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,32 +19,59 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    /**
+     * Define reglas con base a las cuales se crea la cadena de filtros de seguridad
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return  httpSecurity
                 .csrf(csrf -> csrf.disable())
                 /**
-                 * Mecánismo de autenticación que estableces que las credenciales del
-                 * usuario se envían en cada request
+                 * Habilita autenticación HTTP Basic.
                  */
                 .httpBasic(Customizer.withDefaults())
                 /**
-                 * Define la política de creación de sesión como STATELESS, lo que significa
-                 * que el servidor no almacena ningún tipo de estado del usuario
+                 * Configura autenticación sin estado (stateless).
                  */
                 .sessionManagement(sesion -> sesion.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                //Inicialmente, definimos a todas las request como publicas
+                //Inicialmente, todas las requests son públicas
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .build();
     }
 
     /**
      * Gestiona los proveedores de autenticación y escoge el más adecuado teniendo
-     * en cuenta al mecánismo de autenticación que se esté usando
+     * en cuenta al mecanismo de autenticación que se esté usando
      */
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Proveedor de autenticación basado en username y password.
+     * */
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        //Define el algoritmo de hash usado para validar contraseñas.
+        provider.setPasswordEncoder(passwordEncoder());
+        //Service usado para cargar los datos del usuario autenticado
+        provider.setUserDetailsService(userDetailsService);
+
+        return provider;
     }
 
 
