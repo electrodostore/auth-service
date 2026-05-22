6d71e17d3fbe66236dae876a2db96a7e164a9389
Emanuel <emanuelatencia2407@gmail.com>
2026-05-21 19:10:59 -0500

    Implementar UserDetailsService  para consultar los detalles de un usuario desde la base de datos

---
+++ src/main/java/com/electrodostore/auth_service/service/UserDetailsServiceImp.java
@@ -0,0 +1,69 @@
+package com.electrodostore.auth_service.service;
+
+import com.electrodostore.auth_service.model.Permission;
+import com.electrodostore.auth_service.model.Role;
+import com.electrodostore.auth_service.model.UserSec;
+import com.electrodostore.auth_service.repository.IUserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    private final IUserRepository userRepo;
+
+    public UserDetailsServiceImp(IUserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    //Valida existencia de usuario y consulta los detalles de este en la base de datos
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //Busca usuario por username y en caso de que no exista lanza AuthenticationException
+        UserSec user = userRepo.findByUsername(username)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("usuario o contraseña incorrecta")
+                );
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        /**
+         * Recorre la lista de roles del usuario y por cada elemento crea un objeto GrantedAuthority con el
+         * prefijo "ROLE_" y el nombre del rol
+         */
+        user.getListRoles().forEach(
+                role -> authorities.add(new SimpleGrantedAuthority("ROLE_".concat(role.getName())))
+        );
+
+        /**
+         * Aplana las listas de permisos de cada rol, formando una única lista, la cual recorre y por cada permiso
+         * crea un objeto GrantedAuthority con el nombre de este y lo agrega a la lista de autoridades
+         */
+        user.getListRoles()
+                .stream()
+                .flatMap(role -> role.getListPermissions().stream())
+                .forEach(
+                    permission -> authorities.add(new SimpleGrantedAuthority(permission.getName()))
+                );
+
+        //Se retornan detaller del usuario
+        return new User(
+                user.getUsername(),
+                user.getPassword(),
+                user.isEnabled(),
+                user.isAccountNotExpired(),
+                user.isCredentialNotExpired(),
+                user.isAccountNotLocked(),
+                authorities
+
+        );
+    }
+}
