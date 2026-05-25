c2e096df0326056e0c7fd2419f10286a8afb3dc3
Emanuel <emanuelatencia2407@gmail.com>
2026-05-24 20:30:47 -0500

    Agregar userId y clientId (si el usuario es cliente) como claims al token JWT

--- src/main/java/com/electrodostore/auth_service/security/jwt/JwtUtils.java
+++ src/main/java/com/electrodostore/auth_service/security/jwt/JwtUtils.java
@@ -2,6 +2,9 @@ package com.electrodostore.auth_service.security.jwt;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.electrodostore.auth_service.exception.UserNotFoundException;
+import com.electrodostore.auth_service.model.UserSec;
+import com.electrodostore.auth_service.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -20,9 +23,11 @@ public class JwtUtils {
     @Value("${jwt.user.generator}")
     private String userGenerator;
 
+    private final IUserRepository userRepo;
     private final KeyLoader keyLoader;
 
-    public JwtUtils(KeyLoader keyLoader) {
+    public JwtUtils(IUserRepository userRepo, KeyLoader keyLoader) {
+        this.userRepo = userRepo;
         this.keyLoader = keyLoader;
     }
 
@@ -41,6 +46,21 @@ public class JwtUtils {
 
         String username = authentication.getName();
 
+        //Consulta datos del usuario
+        UserSec user = userRepo.findByUsername(username)
+                .orElseThrow(
+                        () -> new UserNotFoundException("No se encontró usuario con username: " + username)
+                );
+
+        /**
+         * Extrae la identidad del usuario para agregarla como claims al JWT.
+         * Si el usuario está asociado a un cliente, también se incluye su clientId.
+         */
+        Long userId = user.getId();
+        Long clientId = null;
+
+        if(user.getClienteId() != null){clientId = user.getClienteId();}
+
         List<String> authorities = authentication.getAuthorities()
                 .stream()
                 .map(GrantedAuthority::getAuthority)
@@ -52,6 +72,8 @@ public class JwtUtils {
         return JWT.create()
                 .withJWTId(UUID.randomUUID().toString())
                 .withSubject(username)
+                .withClaim("userId", userId)
+                .withClaim("clientId", clientId)
                 .withIssuer(userGenerator)
                 .withIssuedAt(Date.from(now))
                 .withExpiresAt(now.plus(Duration.ofMinutes(30)))
