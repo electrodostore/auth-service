a295ceca506974de5c7e56818196e31f2acc0a9b
Emanuel <emanuelatencia2407@gmail.com>
2026-05-29 12:49:10 -0500

    fix: actualizar rutas de integración feign con cliente-service

--- src/main/java/com/electrodostore/auth_service/integration/cliente/client/ClienteFeignClient.java
+++ src/main/java/com/electrodostore/auth_service/integration/cliente/client/ClienteFeignClient.java
@@ -2,10 +2,7 @@ package com.electrodostore.auth_service.integration.cliente.client;
 
 import com.electrodostore.auth_service.dto.user.ClientRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Cliente Feign para comunicación con cliente-service
@@ -17,6 +14,6 @@ public interface ClienteFeignClient {
     @PostMapping("/clientes")
     Long saveCliente(@RequestBody ClientRequestDto newCliente);
 
-    @DeleteMapping("/clientes/{clientId}")
+    @PatchMapping("/clientes/{clientId}/disable")
     void disableClient(@PathVariable Long clientId);
 }
