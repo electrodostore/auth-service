18b1c7311c03b3c617828d7ebe7de5a2f47ffe5b
Emanuel <emanuelatencia2407@gmail.com>
2026-06-04 12:56:48 -0500

    refactor: permitir lista de roles vacía en RoleRequestDto

--- src/main/java/com/electrodostore/auth_service/dto/role/RoleRequestDto.java
+++ src/main/java/com/electrodostore/auth_service/dto/role/RoleRequestDto.java
@@ -2,9 +2,10 @@ package com.electrodostore.auth_service.dto.role;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record RoleRequestDto(
         @NotBlank String roleName,
-        @NotEmpty List<@NotBlank String> permissions){}
+        @NotNull List<@NotBlank String> permissions){}
