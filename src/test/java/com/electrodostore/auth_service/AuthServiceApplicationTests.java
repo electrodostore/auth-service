af0fb89d2afc75b19402218e0a415b955ea7dec9
Emanuel <emanuelatencia2407@gmail.com>
2026-06-08 09:13:00 -0500

    test: configurar entorno de pruebas y corregir levantamiento de contexto en auth-service

--- src/test/java/com/electrodostore/auth_service/AuthServiceApplicationTests.java
+++ src/test/java/com/electrodostore/auth_service/AuthServiceApplicationTests.java
@@ -1,13 +1,20 @@
-//package com.electrodostore.auth_service;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class AuthServiceApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.electrodostore.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AuthServiceApplicationTests {
+
+	@MockitoBean
+	private JwtDecoder jwtDecoder;
+
+	@Test
+	void contextLoads() {
+	}
+
+}
